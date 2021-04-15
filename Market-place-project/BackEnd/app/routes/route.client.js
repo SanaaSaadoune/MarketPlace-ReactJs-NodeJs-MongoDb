4a9cd607897c0222386aae2cd443a468703fd904
Saadoune <sanaasdn@hotmail.com>
2021-04-15 13:33:33 +0000

    Client part

--- Market-place-project/BackEnd/app/routes/route.client.js
+++ Market-place-project/BackEnd/app/routes/route.client.js
@@ -5,56 +5,9 @@ module.exports = app => {
     const middleWare = require("../middleware/authenticateAdmin")
 
 
-    Router.route("/login").post([
-        body('email')
-        .not().isEmpty()
-        .withMessage('please enter email')
-        .isEmail()
-        .withMessage('must be an email')
-        .isLength({ min: 5 })
-        .withMessage('must be at least 5 chars long'),
-        
-        body('password')
-        .not().isEmpty()
-        .withMessage('please enter Password')
-        .isLength({ min: 4 })
-        .withMessage('must be at least 4 chars long')
+    Router.route("/login").post(conntrollerClient.loginClient);
 
-    ],conntrollerClient.loginClient);
-
-    Router.route("/register").post([
-        body('nom')
-        .not().isEmpty()
-        .withMessage('please enter name'),
-
-        body('prenom')
-        .not().isEmpty()
-        .withMessage('please enter prenom'),
-        
-        body('email')
-        .not().isEmpty()
-        .withMessage('please enter email')
-        .isEmail()
-        .withMessage('must be an email'),
-
-        body('password')
-        .not().isEmpty()
-        .withMessage('please enter password')
-        .isLength({ min: 4 })
-        .withMessage('must be at least 4 chars long')
-        .matches(/\d/)
-        .withMessage('must contain a number'),
-
-        body('phone')
-        .not().isEmpty() 
-        .withMessage('please enter phone')
-        .isMobilePhone()
-        .withMessage('please enter correct mobile phone'),
-
-        body('adresse')
-        .not().isEmpty()
-        .withMessage('please enter adresse'),
-    ],conntrollerClient.registerClient);
+    Router.route("/register").post(conntrollerClient.registerClient);
 
     Router.route("/confirm/:confirmationCode").get(conntrollerClient.verifyClient);
     Router.route("/").get(middleWare.admin,conntrollerClient.getClient);
